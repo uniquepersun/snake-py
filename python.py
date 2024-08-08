8f04e3a093ae8456a002db1a0ea6dfe11701f758
abhay <abhay.i0am@gmail.com>
2024-08-08 13:25:22 +0530

    removed bug, color --> Color and etc...

--- python.py
+++ python.py
@@ -8,12 +8,19 @@ difficulty = 25
 f_size_x = 720
 f_size_y = 480
 
+c_err = pygame.init()
+if c_err[1] > 0:
+    print(f"[!] had {c_err[1]} errors when initiating a game, closing..") 
+    sys.exit(-1)
+else:
+    print("[*] game initiated")
+
 pygame.display.set_caption("snake.python")
 game_window = pygame.display.set_mode((f_size_x,f_size_y))
 
-black = pygame.color(0,0,0)
-white = pygame.color(255,255,255)
-red = pygame.color(255,0,0)
+black = pygame.Color(0,0,0)
+white = pygame.Color(255,255,255)
+red = pygame.Color(255,0,0)
 green = pygame.Color(0, 255, 0)
 blue = pygame.Color(0, 0, 255)
 
@@ -28,6 +35,33 @@ direction = 'RIGHT'
 change_to = direction
 
 score = 0
+
+
+def game_over():
+    my_font = pygame.font.SysFont('times new roman', 90)
+    game_over_surface = my_font.render('YOU DIED', True, red)
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (f_size_x/2, f_size_y/4)
+    game_window.fill(black)
+    game_window.blit(game_over_surface, game_over_rect)
+    show_score(0, red, 'times', 20)
+    pygame.display.flip()
+    time.sleep(3)
+    pygame.quit()
+    sys.exit()
+
+
+
+def show_score(choice, color, font, size):
+    score_font = pygame.font.SysFont(font, size)
+    score_surface = score_font.render('Score : ' + str(score), True, color)
+    score_rect = score_surface.get_rect()
+    if choice == 1:
+        score_rect.midtop = (f_size_x/10, 15)
+    else:
+        score_rect.midtop = (f_size_x/2, f_size_y/1.25)
+    game_window.blit(score_surface, score_rect)
+   
 
 
 
@@ -80,7 +114,7 @@ while True:
 
    
     if not food_spawn:
-        food_pos = [random.randrange(1, (frame_size_x//10)) * 10, random.randrange(1, (frame_size_y//10)) * 10]
+        food_pos = [random.randrange(1, (f_size_x//10)) * 10, random.randrange(1, (f_size_y//10)) * 10]
     food_spawn = True
 
    
@@ -93,9 +127,9 @@ while True:
     pygame.draw.rect(game_window, white, pygame.Rect(food_pos[0], food_pos[1], 10, 10))
 
    
-    if snake_pos[0] < 0 or snake_pos[0] > frame_size_x-10:
+    if snake_pos[0] < 0 or snake_pos[0] > f_size_x-10:
         game_over()
-    if snake_pos[1] < 0 or snake_pos[1] > frame_size_y-10:
+    if snake_pos[1] < 0 or snake_pos[1] > f_size_y-10:
         game_over()
     
     for block in snake_body[1:]:
